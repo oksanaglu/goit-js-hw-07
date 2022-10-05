06352361c527603fbfdbedde9a68413d95fd8879
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-05 19:43:25 +0300

    task01-hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+for (let el of galleryItems) 
+document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<div class="gallery__item"><a class="gallery__link"><img class="gallery__image" src ="${el.preview}" data-source = "${el.original}" alt ="${el.description}"></a></div>`);
 
 console.log(galleryItems);
 
