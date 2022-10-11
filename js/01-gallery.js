8b8c56e0d537d693d8cd6a43a98ce2f2c49c8aad
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-11 12:16:16 +0300

    hw7-js-correct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,74 @@
 import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
-for (let el of galleryItems) 
-document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<div class="gallery__item"><a class="gallery__link" href="${el.original}"><img class="gallery__image" src ="${el.preview}" data-source = "${el.original}" alt ="${el.description}"></a></div>`);
+// for (let el of galleryItems) 
+// document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<div class="gallery__item"><a class="gallery__link" href="${el.original}"><img class="gallery__image" src ="${el.preview}" data-source = "${el.original}" alt ="${el.description}"></a></div>`);
 
 
-const galleryContainer = document.querySelector(".gallery");
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+// const galleryContainer = document.querySelector(".gallery");
+// galleryContainer.addEventListener('click', onGalleryContainerClick, false);
+
+// function onGalleryContainerClick(event) {
+//    event.preventDefault();
+//   if (!event.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+  
+//   const instance = basicLightbox.create
+//     (`<img src="${event.target.dataset.source}">`)
+//   instance.show()
+
+//   galleryContainer.addEventListener('keydown', e => {
+//     if (e.key === 'Escape') {
+//       instance.close()
+//     }
+//   });
+// }
+
+
+const galleryContainerEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainerEl.addEventListener('click', onGalleryContainerClick);
+
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}" >
+    <img
+      class="gallery__image"
+              src="${preview}"
+              data-source="${original}" 
+              alt="${description}"
+    />
+  </a>
+</div>
+       `;
+    })
+    .join('');
+}
 
 function onGalleryContainerClick(event) {
-   event.preventDefault();
+    event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-  
-  const instance = basicLightbox.create
+  // console.log(event.target);
+ const instance = basicLightbox.create
     (`<img src="${event.target.dataset.source}">`)
   instance.show()
-  galleryContainer.addEventListener('keydown', e => {
+
+  // const galleryEl = event.target;
+  // const parentGalleryEl = galleryEl.closest(".gallery__item");
+  // console.log(parentGalleryEl);
+  
+  galleryContainerEl.addEventListener('keydown', e => {
     if (e.key === 'Escape') {
       instance.close()
     }
-  })
+  });
 }
-
-
