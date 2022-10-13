6c3be98d5f0aa2a0229452daebdf3fa76acd4394
Oksana Gluschenko <oksana_a_g@ukr.net>
2022-10-13 20:21:49 +0300

    hw7-js-correct-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-console.log(galleryItems);
+
 
 // for (let el of galleryItems) 
 // document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<div class="gallery__item"><a class="gallery__link" href="${el.original}"><img class="gallery__image" src ="${el.preview}" data-source = "${el.original}" alt ="${el.description}"></a></div>`);
@@ -25,6 +25,7 @@ console.log(galleryItems);
 //   });
 // }
 
+// ========================================================================================================
 
 const galleryContainerEl = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
@@ -72,3 +73,64 @@ function onGalleryContainerClick(event) {
     }
   });
 }
+
+// ==============================================================================================================
+
+// const galleryContainerEl = document.querySelector(".gallery");
+// const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+// function createGalleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+// <div class="gallery__item">
+//   <a class="gallery__link" href="${original}" >
+//     <img
+//       class="gallery__image"
+//               src="${preview}"
+//               data-source="${original}" 
+//               alt="${description}"
+//     />
+//   </a>
+// </div>
+//        `;
+//     })
+//     .join('');
+// }
+
+// galleryContainerEl.addEventListener('click', onGalleryContainerClick);
+// function onGalleryContainerClick(event) {
+//     event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   createModalWindow(event.target.dataset.source);
+
+// }
+
+// let instance;
+// function createModalWindow(url) {
+//   const instance = basicLightbox.create
+//     (`<img src="${url}">`)
+//   instance.show();
+//   document.addEventListener('keydown', onEscClick);
+//   closeByClick();
+// }
+
+// function onEscClick(event) {
+// if (event.code === 'Escape') {
+//   instance.close();
+//     }
+//   document.removeEventListener('keydown', onEscClick);
+// }
+ 
+// function closeByClick() {
+//   const basicLightboxRef = document.querySelector('.basicLightbox');
+//   basicLightboxRef.addEventListener('click', onEscClick);
+// }
+
+console.log(galleryItems);
